c17252175bd465c66a7de9a4611113d7cb06a75d
Strahinja Bozovic <straleb123@gmail.com>
2025-06-30 22:56:34 +0200

    sveeeee

--- tenisApp/src/main/java/com/example/tenisApp/security/CorsConfig.java
+++ tenisApp/src/main/java/com/example/tenisApp/security/CorsConfig.java
@@ -10,13 +10,12 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("*")  // Dozvoli sve origin-e
+                        .allowedMethods("*") // Dozvoli sve HTTP metode
+                        .allowedHeaders("*") // Dozvoli sve headere
+                        .allowCredentials(false);
             }
         };
     }
