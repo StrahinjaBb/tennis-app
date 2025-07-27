d8e475a50f1ac4f6a9dc52e8f3ca7ed33c62ffd5
Strahinja Bozovic <straleb123@gmail.com>
2025-07-27 20:24:01 +0200

    production

--- tennis-app/src/api/config.js
+++ tennis-app/src/api/config.js
@@ -1,6 +1,6 @@
 // Ili pojedinačne konstante ako preferirate
-// export const API_BASE_URL = "https://tennis-app-qh39.onrender.com";
-export const API_BASE_URL = "127.0.0.1:8080";
+export const API_BASE_URL = "https://tennis-app-qh39.onrender.com";
+// export const API_BASE_URL = "127.0.0.1:8080";
 export const AUTH_API_URL = `${API_BASE_URL}/auth`;
 export const USERS_API_URL = `${API_BASE_URL}/users`;
 export const APPOINTMENTS_API_URL = `${API_BASE_URL}/appointments`;
